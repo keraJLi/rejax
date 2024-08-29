0507195f65054f53c022d27237ea68dee17a0701
jarek <jarek@bccn-berlin.de>
2024-08-29 19:11:45 +0200

    fixed td3 test

--- test/test_td3.py
+++ test/test_td3.py
@@ -29,68 +29,68 @@ class TestEnvironmentsTD3(unittest.TestCase):
     def train_fn(self, td3):
         return TD3.train(td3, rng=jax.random.PRNGKey(0))
 
-    # def test_env1(self):
-    #     env = TestEnv1Continuous()
-    #     td3 = TD3.create(env=env, **self.args)
-    #     ts, _ = self.train_fn(td3)
-    #     act = td3.make_act(ts)
+    def test_env1(self):
+        env = TestEnv1Continuous()
+        td3 = TD3.create(env=env, **self.args)
+        ts, _ = self.train_fn(td3)
+        act = td3.make_act(ts)
 
-    #     rng = jax.random.PRNGKey(0)
-    #     rngs = jax.random.split(rng, 10)
-    #     obs = jax.numpy.zeros((10, 1))
-    #     actions = jax.vmap(act)(obs, rngs)
-        
-    #     actions = jax.numpy.expand_dims(actions, 1)
-    #     q_fn = jax.vmap(td3.critic.apply, in_axes=(0, None, None))
+        rng = jax.random.PRNGKey(0)
+        rngs = jax.random.split(rng, 10)
+        obs = jax.numpy.zeros((10, 1))
+        actions = jax.vmap(act)(obs, rngs)
 
-    #     qs = q_fn(ts.critic_ts.params, obs, actions)
-    #     value = qs.min(axis=0)
+        actions = jax.numpy.expand_dims(actions, 1)
+        q_fn = jax.vmap(td3.critic.apply, in_axes=(0, None, None))
 
-    #     for v in value:
-    #         self.assertAlmostEqual(v, 1.0, delta=0.1)
+        qs = q_fn(ts.critic_ts.params, obs, actions)
+        value = qs.min(axis=0)
 
-    # def test_env2(self):
-    #     env = TestEnv2Continuous()
-    #     td3 = TD3.create(env=env, **self.args)
-    #     ts, _ = self.train_fn(td3)
-    #     act = td3.make_act(ts)
+        for v in value:
+            self.assertAlmostEqual(v, 1.0, delta=0.1)
 
-    #     rng = jax.random.PRNGKey(0)
-    #     rngs = jax.random.split(rng, 10)
-    #     obs = jax.random.uniform(rng, (10, 1), minval=-1, maxval=1)
-    #     actions = jax.vmap(act)(obs, rngs)
-    #     actions = jax.numpy.expand_dims(actions, 1)
-    #     q_fn = jax.vmap(td3.critic.apply, in_axes=(0, None, None))
+    def test_env2(self):
+        env = TestEnv2Continuous()
+        td3 = TD3.create(env=env, **self.args)
+        ts, _ = self.train_fn(td3)
+        act = td3.make_act(ts)
 
-    #     qs = q_fn(ts.critic_ts.params, obs, actions)
-    #     value = qs.min(axis=0)
+        rng = jax.random.PRNGKey(0)
+        rngs = jax.random.split(rng, 10)
+        obs = jax.random.uniform(rng, (10, 1), minval=-1, maxval=1)
+        actions = jax.vmap(act)(obs, rngs)
+        actions = jax.numpy.expand_dims(actions, 1)
+        q_fn = jax.vmap(td3.critic.apply, in_axes=(0, None, None))
 
-    #     for v, r in zip(value, obs):
-    #         self.assertAlmostEqual(v, r, delta=0.1)
+        qs = q_fn(ts.critic_ts.params, obs, actions)
+        value = qs.min(axis=0)
 
-    # def test_env3(self):
-    #     env = TestEnv3Continuous()
-    #     td3 = TD3.create(env=env, **self.args)
-    #     ts, _ = self.train_fn(td3)
-    #     act = td3.make_act(ts)
-    #     q_fn = jax.vmap(td3.critic.apply, in_axes=(0, None, None))
+        for v, r in zip(value, obs):
+            self.assertAlmostEqual(v, r, delta=0.1)
 
-    #     @partial(jax.vmap, in_axes=(None, 0))
-    #     def test_i(obs, rng):
-    #         action = act(obs, rng)
-    #         action = jax.numpy.expand_dims(action, 0)
-    #         obs = jax.numpy.expand_dims(obs, 0)
-    #         action = jax.numpy.expand_dims(action, 1)
+    def test_env3(self):
+        env = TestEnv3Continuous()
+        td3 = TD3.create(env=env, **self.args)
+        ts, _ = self.train_fn(td3)
+        act = td3.make_act(ts)
+        q_fn = jax.vmap(td3.critic.apply, in_axes=(0, None, None))
 
-    #         qs = q_fn(ts.critic_ts.params, obs, action)
-    #         value = qs.min(axis=0)
-    #         return value
+        @partial(jax.vmap, in_axes=(None, 0))
+        def test_i(obs, rng):
+            action = act(obs, rng)
+            action = jax.numpy.expand_dims(action, 0)
+            obs = jax.numpy.expand_dims(obs, 0)
+            action = jax.numpy.expand_dims(action, 1)
 
-    #     rngs = jax.random.split(jax.random.PRNGKey(0), 10)
-    #     for obs in jax.numpy.array([[-1], [1]]):
-    #         r = 1 * td3.gamma if obs == -1 else 1
-    #         for v in test_i(obs, rngs):
-    #             self.assertAlmostEqual(v, r, delta=0.1)
+            qs = q_fn(ts.critic_ts.params, obs, action)
+            value = qs.min(axis=0)
+            return value
+
+        rngs = jax.random.split(jax.random.PRNGKey(0), 10)
+        for obs in jax.numpy.array([[-1], [1]]):
+            r = 1 * td3.gamma if obs == -1 else 1
+            for v in test_i(obs, rngs):
+                self.assertAlmostEqual(v, r, delta=0.1)
 
     def test_env4(self):
         env = TestEnv4Continuous()
@@ -102,7 +102,11 @@ class TestEnvironmentsTD3(unittest.TestCase):
         @partial(jax.vmap, in_axes=(None, 0))
         def test_i(obs, rng):
             action = act(obs, rng)
-            qs = q_fn(ts.critic_ts.params, obs[None, :], action[None, None, :])
+            action = jax.numpy.expand_dims(action, 0)
+            obs = jax.numpy.expand_dims(obs, 0)
+            action = jax.numpy.expand_dims(action, 1)
+
+            qs = q_fn(ts.critic_ts.params, obs, action)
             value = qs.min(axis=0)
             return value, action
 
