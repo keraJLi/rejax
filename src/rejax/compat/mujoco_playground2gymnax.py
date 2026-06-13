70d98451ef4d151a813fae2ee86ab65118783ca7
letoku <jakub.tomasz.lewandowski@gmail.com>
2026-06-13 19:14:14 +0200

    Add: mujoco_playground wrapper

---
+++ src/rejax/compat/mujoco_playground2gymnax.py
@@ -0,0 +1,72 @@
+import warnings
+from copy import copy
+
+from flax import struct
+from gymnax.environments import spaces
+from gymnax.environments.environment import Environment as GymnaxEnv
+from jax import numpy as jnp
+import mujoco_playground
+
+
+def create_mujoco_playground(env_name, **kwargs):
+    env = mujoco_playground.registry.load(env_name)
+    cfg = mujoco_playground.registry.get_default_config(env_name)
+    env = MujocoPlayground2GymnaxEnv(env, cfg)
+    return env, env.default_params
+
+
+@struct.dataclass
+class EnvParams:
+    # CAUTION: Passing params with a different value than on init has no effect
+    max_steps_in_episode: int = 1000
+
+
+class MujocoPlayground2GymnaxEnv(GymnaxEnv):
+    def __init__(self, env, cfg):
+        self.env = env
+        self.max_steps_in_episode = cfg.episode_length
+
+    @property
+    def default_params(self):
+        return EnvParams(max_steps_in_episode=self.max_steps_in_episode)
+
+    def step_env(self, key, state, action, params):
+        state = self.env.step(state, action)
+        return state.obs, state, state.reward, state.done.astype(bool), state.info
+
+    def reset_env(self, key, params):
+        state = self.env.reset(key)
+        return state.obs, state
+
+    def get_obs(self, state):
+        return state.obs
+
+    def is_terminal(self, state):
+        return state.done.astype(bool)
+
+    @property
+    def name(self):
+        return self.env.unwrapped.__class__.__name__
+
+    def action_space(self, params):
+        # All mjx evironments should have action limit of -1 to 1
+        return spaces.Box(low=-1, high=1, shape=(self.env.action_size,))
+
+    def observation_space(self, params):
+        # All mjx evironments should have observation limit of -inf to inf
+        return spaces.Box(
+            low=-jnp.inf, high=jnp.inf, shape=(self.env.observation_size,)
+        )
+
+    @property
+    def num_actions(self) -> int:
+        return self.env.action_size
+
+    def __deepcopy__(self, memo):
+        warnings.warn(
+            f"Trying to deepcopy {type(self).__name__}, which contains a mujoco_playground env,"
+            "so a shallow copy is returned.",
+            category=RuntimeWarning,
+            stacklevel=2,
+        )
+        return copy(self)
